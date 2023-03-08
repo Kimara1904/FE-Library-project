d459646c0cae8bd9c513b7adbc7d86729ba88f5f
Miki Markovic <komadant@outlook.com>
2023-03-08 12:50:19 +0100

    Modify login validation and and login display

--- fe-library-app/src/components/AppRouter/pages/LoginPage.tsx
+++ fe-library-app/src/components/AppRouter/pages/LoginPage.tsx
@@ -1,23 +1,30 @@
 import { createRef, useState } from 'react'
 
 import { useNavigate } from 'react-router'
+import axios, { AxiosError } from 'axios'
 
 import { login } from '../../../services/AuthService'
 import styles from './LoginPage.module.css'
 
 const LoginPage = () => {
-  const [ canSubmit, setCanSubmit ] = useState(false)
+  const [ emailError, setEmailError ] = useState(false)
+  const [ passwordError, setPasswordError ] = useState(false)
+  const [ loginError, setLoginError ] = useState(false)
 
   const navigation = useNavigate()
 
   const emailRef = createRef<HTMLInputElement>()
   const passwordRef = createRef<HTMLInputElement>()
 
-  const checkInputs = () => {
-    if (emailRef.current?.value.trim().length !== 0 && passwordRef.current?.value.trim().length !== 0){
-      setCanSubmit(true)
-    }else{
-      setCanSubmit(false)
+  const handleBlurEmail = () => {
+    if (emailRef.current?.value.trim().length === 0) {
+      setEmailError(true)
+    }
+  }
+
+  const handleBlurPassword = () =>{
+    if (passwordRef.current?.value.trim().length === 0) {
+      setPasswordError(true)
     }
   }
 
@@ -27,13 +34,25 @@ const LoginPage = () => {
     const enteredEmail = emailRef.current?.value as string
     const enteredPassword = passwordRef.current?.value as string
 
+    if (enteredEmail.trim().length === 0) {
+      setEmailError(true)
+      return
+    }
+
+    if (enteredPassword.trim().length === 0) {
+      setPasswordError(true)
+      return
+    }
+
     login(enteredEmail, enteredPassword)
       .then((response) => {
         sessionStorage.setItem('token', response.data.accessToken)
         navigation('/home')
       })
-      .catch(() => {
-        alert('Wrong email or password!')
+      .catch((error: AxiosError) => {
+        if (axios.isAxiosError(error)) {
+          setLoginError(true)
+        }
       })
   }
 
@@ -41,14 +60,19 @@ const LoginPage = () => {
     <div className={styles.login_form}>
       <form onSubmit={submitHandler}>
         <div>
-          <label>Email</label>
-          <input type='text' name='email' ref={emailRef} onChange={checkInputs}/>
+          <label className={styles.login_label_desc}>Email</label>
+          <input className={emailError ? styles.login_input_invalid : ''} type='text' name='email' ref={emailRef} onBlur={handleBlurEmail} onFocus={() => setEmailError(false)}/>
         </div>
+        <label className={!emailError ? styles.success_login_input : styles.err_login_input}>Email is required</label>
         <div>
-          <label>Password</label>
-          <input type='password' name='password' ref={passwordRef} onChange={checkInputs} />
+          <label className={styles.login_label_desc}>Password</label>
+          <input className={passwordError ? styles.login_input_invalid : ''} type='password' name='password' ref={passwordRef} onBlur={handleBlurPassword} onFocus={() => setPasswordError(false)}/>
         </div>
-        <button className={canSubmit ? styles.login_button : styles.login_button_unable}>Login</button>
+        <label className={!passwordError ? styles.success_login_input : styles.err_login_input}>Password is required</label>
+        <div className={styles.form_div_button_label_error}>
+          <label className={!loginError ? styles.success_login_credentials : styles.err_login_credentials}>Wrong credentials</label>
+          <button className={styles.login_button}>Login</button>
+        </div>
       </form>
     </div>
   )
