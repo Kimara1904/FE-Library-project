136cb8908f02f7fd5f504b7399acbfcc5d7ad042
Miki Markovic <komadant@outlook.com>
2023-03-21 15:20:29 +0100

    Implement book details for desktop

--- fe-library-app/src/components/BookDetail/BookDetail.tsx
+++ fe-library-app/src/components/BookDetail/BookDetail.tsx
@@ -45,71 +45,88 @@ const BookDetail = () => {
   }
 
   return (
-    <div className={style.book_details}>
-      <img
-        src={
-          book?.Cover !== '' && book?.Cover != null
-            ? 'data:image/png;base64,' + book?.Cover
-            : DefaultBookCover
-        }
-        alt='Book cover'
-      />
-      <table>
-        <tbody>
-          <tr>
-            <th>Title</th>
-            <td>{book?.Title}</td>
-          </tr>
-          <tr>
-            <th>Description</th>
-            <td>{book?.Description}</td>
-          </tr>
-          <tr>
-            <th>Isbn</th>
-            <td>{book?.ISBN}</td>
-          </tr>
-          <tr>
-            <th>Quantity</th>
-            <td>{book?.Quantity}</td>
-          </tr>
-          <tr>
-            <th>Available</th>
-            <td>{book?.Available}</td>
-          </tr>
-          <tr>
-            <th>Publish date</th>
-            <td>{book?.PublishDate != null ? new Date(book?.PublishDate).toDateString() : ''}</td>
-          </tr>
-          <tr>
-            <th rowSpan={book?.Authors.length !== 0 ? book?.Authors.length : 1}>Authors</th>
-            {book?.Authors.length === 0 ? (
-              <td />
-            ) : (
-              <td>
-                {(book?.Authors[0].Firstname as string) +
-                  ' ' +
-                  (book?.Authors[0].Lastname as string)}
-              </td>
-            )}
-          </tr>
-          {(book?.Authors.length as number) - 1 !== 0 && (
-            <>
-              {book?.Authors.slice(1).map((author) => (
-                <tr key={author.Id}>
+    <div className={style.book_details_rent_history}>
+      <h1>Book Detail:</h1>
+      <div className={style.book_details}>
+        <img
+          src={
+            book?.Cover !== '' && book?.Cover != null
+              ? 'data:image/png;base64,' + book?.Cover
+              : DefaultBookCover
+          }
+          alt='Book cover'
+        />
+        <div>
+          <table>
+            <tbody>
+              <tr>
+                <th>Title</th>
+                <td>{book?.Title}</td>
+              </tr>
+              <tr>
+                <th>Description</th>
+                <td>{book?.Description}</td>
+              </tr>
+              <tr>
+                <th>Isbn</th>
+                <td>{book?.ISBN}</td>
+              </tr>
+              <tr>
+                <th>Quantity</th>
+                <td>{book?.Quantity}</td>
+              </tr>
+              <tr>
+                <th>Available</th>
+                <td>{book?.Available}</td>
+              </tr>
+              <tr>
+                <th>Publish date</th>
+                <td>
+                  {book?.PublishDate != null ? new Date(book?.PublishDate).toDateString() : ''}
+                </td>
+              </tr>
+              <tr>
+                <th className={style.book_details_author_th}rowSpan={book?.Authors.length !== 0 ? book?.Authors.length : 1}>Authors</th>
+                {book?.Authors.length === 0 ? (
+                  <td />
+                ) : (
                   <td>
-                    {author.Firstname} {author.Lastname}
+                    {(book?.Authors[0].Firstname as string) +
+                      ' ' +
+                      (book?.Authors[0].Lastname as string)}
                   </td>
-                </tr>
-              ))}
-            </>
+                )}
+              </tr>
+              {(book?.Authors.length as number) - 1 !== 0 && (
+                <>
+                  {book?.Authors.slice(1).map((author) => (
+                    <tr key={author.Id}>
+                      <td>
+                        {author.Firstname} {author.Lastname}
+                      </td>
+                    </tr>
+                  ))}
+                </>
+              )}
+            </tbody>
+          </table>
+          {showModifyModal && (
+            <CreateUpdateBookModal
+              onCreateOrModifySuccess={() => getBookInfo()}
+              onHideModal={() => setShowModifyModule(false)}
+              id={parseInt(id as string)}
+            />
           )}
-        </tbody>
-      </table>
-      {showModifyModal && <CreateUpdateBookModal onCreateOrModifySuccess={() => getBookInfo} onHideModal={() => setShowModifyModule(false)} />}
-      <div className={style.book_details_buttons}>
-        <button className={style.book_modify_phone} onClick={handleModifyBookClick}>Modify</button>
-        <button className={style.book_modify_desktop} onClick={() => setShowModifyModule(false)}>Modify</button>
-        <button onClick={handleBookDelete}>Delete</button>
+          <div className={style.book_details_buttons}>
+            <button className={style.book_modify_phone} onClick={handleModifyBookClick}>
+              Modify
+            </button>
+            <button className={style.book_modify_desktop} onClick={() => setShowModifyModule(true)}>
+              Modify
+            </button>
+            <button onClick={handleBookDelete}>Delete</button>
+          </div>
+        </div>
       </div>
     </div>
   )
